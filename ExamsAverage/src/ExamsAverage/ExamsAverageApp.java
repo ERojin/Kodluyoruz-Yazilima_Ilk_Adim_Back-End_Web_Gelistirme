80f621c1ccc5efb174ba701378eb415a4d2a0740
Eylul Rojin Yılmaz <97739030+ERojin@users.noreply.github.com>
2023-10-26 23:09:30 +0300

    Update ExamsAverageApp.java

--- ExamsAverage/src/ExamsAverage/ExamsAverageApp.java
+++ ExamsAverage/src/ExamsAverage/ExamsAverageApp.java
@@ -1,3 +1,12 @@
+/*-------------------------------------------------------------------------------------------------------
+Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan
+ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
+
+Alıştırma
+Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise
+ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
+Not : If ve Else kullanılmayacak...
+ --------------------------------------------------------------------------------------------------------*/
 package ExamsAverage;
 
 import java.util.Scanner;
