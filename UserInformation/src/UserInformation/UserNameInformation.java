433237d18762c50da2e61dc60ac6d694ad8d8640
Eylul Rojin Yılmaz <97739030+ERojin@users.noreply.github.com>
2023-10-27 02:10:06 +0300

    Update UserNameInformation.java

--- UserInformation/src/UserInformation/UserNameInformation.java
+++ UserInformation/src/UserInformation/UserNameInformation.java
@@ -1,3 +1,12 @@
+/*-------------------------------------------------------------------------------------------------------
+Koşullu ifadeler ile kullanıcı adı ve şifreyi kontrol eden program.
+
+Alıştırma
+Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun, eğer kullanıcı sıfırlamak
+isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre ile aynı olmaması gerektiğini kontrol edip ,
+şifreler aynı ise ekrana "Şifre oluşturulamadı, lütfen başka şifre giriniz."
+sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
+ --------------------------------------------------------------------------------------------------------*/
 package UserInformation;
 
 import java.util.Scanner;
