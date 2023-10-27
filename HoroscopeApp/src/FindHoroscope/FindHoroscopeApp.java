cc9027f7ab5dc7fc61456ea4ab0858c65db12b40
Eylul Rojin <eylulrojingunduz@gmail.com>
2023-10-28 01:35:46 +0300

    Horoscope was updated.

--- HoroscopeApp/src/FindHoroscope/FindHoroscopeApp.java
+++ HoroscopeApp/src/FindHoroscope/FindHoroscopeApp.java
@@ -1,11 +1,18 @@
 /*-------------------------------------------------------------------------------------------------------
-Koşullu ifadeler ile kullanıcı adı ve şifreyi kontrol eden program.
+Java koşullu ifadeler ile kullanıcının burcunu bulan program yazınız.
 
-Alıştırma
-Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun, eğer kullanıcı sıfırlamak
-isterse yeni girdiği şifrenin hatalı girdiği ve unuttuğu şifre ile aynı olmaması gerektiğini kontrol edip ,
-şifreler aynı ise ekrana "Şifre oluşturulamadı, lütfen başka şifre giriniz."
-sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
+Koç Burcu : 21 Mart - 20 Nisan
+Boğa Burcu : 21 Nisan - 21 Mayıs
+İkizler Burcu : 22 Mayıs - 22 Haziran
+Yengeç Burcu : 23 Haziran - 22 Temmuz
+Aslan Burcu : 23 Temmuz - 22 Ağustos
+Başak Burcu : 23 Ağustos - 22 Eylül
+Terazi Burcu : 23 Eylül - 22 Ekim
+Akrep Burcu : 23 Ekim - 21 Kasım
+Yay Burcu : 22 Kasım - 21 Aralık
+Oğlak Burcu : 22 Aralık - 21 Ocak
+Kova Burcu : 22 Ocak - 19 Şubat
+Balık Burcu : 20 Şubat - 20 Mart
  --------------------------------------------------------------------------------------------------------*/
 package FindHoroscope;
 
